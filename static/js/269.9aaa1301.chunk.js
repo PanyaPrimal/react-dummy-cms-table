d31b372e773532cfa2c963a67fdfcb675b7ca9cc
Victor P <vpanya97@gmail.com>
2023-10-31 12:03:57 +0200

    Updates

---
+++ static/js/269.9aaa1301.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_dummy_cms_table=self.webpackChunkreact_dummy_cms_table||[]).push([[269],{269:function(e,s,c){c.r(s);c(791);var r=c(184);s.default=function(){return(0,r.jsx)("div",{children:(0,r.jsxs)("table",{children:[(0,r.jsxs)("tr",{children:[(0,r.jsx)("td",{children:"Dummy"}),(0,r.jsx)("td",{children:"Table"})]}),(0,r.jsxs)("tr",{children:[(0,r.jsx)("td",{children:"Dummy"}),(0,r.jsx)("td",{children:"Table"})]})]})})}}}]);
+//# sourceMappingURL=269.9aaa1301.chunk.js.map
